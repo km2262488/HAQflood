582be0e271b77d29ac1900f6da24c722c73d7479
km2262488 <km2262488@gmail.com>
2026-06-23 22:14:50 +0700

    update

--- haq.go
+++ haq.go
@@ -378,9 +378,9 @@ func main() {
 	if state.debugMode {
 		fmt.Println("Debug mode aktif")
 	}
-   fmt.Println(" ▖▖▄▖▄▖  ▄▖▖ ▄▖▄▖▄   ▄▖▄▖  ▄▖▄▖▄▖▄▖▄▖")
+   fmt.Println("  ▖▖▄▖▄▖  ▄▖▖ ▄▖▄▖▄   ▄▖▄▖  ▄▖▄▖▄▖▄▖▄▖")
    fmt.Println(" ▙▌▌▌▌▌  ▙▖▌ ▌▌▌▌▌▌ ▐  ▚    ▚  ▐ ▌▌▙▘▐    ")
-   fmt.Println(" ▌▌▛▌█▌  ▌ ▙▖▙▌▙▌▙▘ ▟▖▄▌   ▄▌▐ ▛▌▌▌▐ ")                       
+   fmt.Println(" ▌▌▛▌█▌  ▌ ▙▖▙▌▙▌▙▘ ▟▖▄▌    ▄▌▐ ▛▌▌▌▐ ")                       
    fmt.Println("                ")
 	var wg sync.WaitGroup
 
